77f694678a9f9d53d86c30162672a6b57239e684
vosoloBradley <vosolobradley@gmail.com>
2024-11-15 13:17:21 +0300

    Dairy farmshop management system final deploy

---
+++ dfsms/includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Developed By<a href="https://www.royalmilks dairy.com" class="text-dark">Bradley Vosolo</a> ©2023</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
